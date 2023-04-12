1905d3723ebb2e2de2ad7827f24c02f448fc9647
jaacko-torus <28635807+jaacko-torus@users.noreply.github.com>
2023-04-12 01:46:16 -0400

    font size change

--- next-react/tailwind.config.js
+++ next-react/tailwind.config.js
@@ -1,6 +1,6 @@
-const { fontFamily } = require('tailwindcss/defaultTheme')
+const { fontFamily } = require("tailwindcss/defaultTheme")
 
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -12,6 +12,21 @@ module.exports = {
 	],
   theme: {
     extend: {
+			fontSize: {
+				"xs": ["1rem", { lineHeight: "1.5rem" }],
+				"sm": ["1.125rem", { lineHeight: "1.75rem" }],
+				"base": ["1.25rem", { lineHeight: "1.75rem" }],
+				"lg": ["1.5rem", { lineHeight: "2rem" }],
+				"xl": ["1.875rem", { lineHeight: "2.25rem" }],
+				"2xl": ["2.25rem", { lineHeight: "2.5rem" }],
+				"3xl": ["3rem", { lineHeight: "1" }],
+				"4xl": ["3.75rem", { lineHeight: "1" }],
+				"5xl": ["4.5rem", { lineHeight: "1" }],
+				"6xl": ["6rem", { lineHeight: "1" }],
+				"7xl": ["8rem", { lineHeight: "1" }],
+				"8xl": ["10rem", { lineHeight: "1" }],
+				"9xl": ["11rem", { lineHeight: "1" }],
+			},
       fontFamily: {
 				"gentium-book-basic": ["var(--font-gentium-book-basic)"],
 				"gentium-book-plus": ["var(--font-gentium-book-plus)"],
@@ -23,6 +38,6 @@ module.exports = {
 		},
   },
 	plugins: [
-		require('tailwindcss-opentype'),
+		require("tailwindcss-opentype"),
 	],
 }
