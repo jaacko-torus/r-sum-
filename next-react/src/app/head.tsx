f74cf3f8660c608af387aba366aabbb27903b58b
jaacko-torus <28635807+jaacko-torus@users.noreply.github.com>
2023-04-24 20:05:50 -0400

    public images, changes

--- next-react/src/app/head.tsx
+++ next-react/src/app/head.tsx
@@ -5,7 +5,7 @@ const Head: React.FC = () =>
 		<title>Julian A. Avar C.&apos;s Resume</title>
 		<meta content="width=device-width, initial-scale=1" name="viewport"/>
 		<meta name="description" content="Julian A. Avar C.'s resume"/>
-		<link rel="icon" href="/favicon_256_32bbp.ico"/>
+		<link rel="icon" href="/favicon.ico"/>
 	</>
 
 export default Head
